96c0b4be6b9e4cb51a57543ffb4020dc42f9f947
emoni1989 <67372685+emoni1989@users.noreply.github.com>
2020-10-01 20:22:08 +0100

    completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,9 @@ import { RouterModule, Routes, RoutesRecognized } from '@angular/router'
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'Products', pathMatch: 'full' }
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 
-];
+]
 
 @NgModule({
   declarations: [
